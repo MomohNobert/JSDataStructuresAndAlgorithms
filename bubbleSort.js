4fe23aece2e31ca6bfa2f2d0ffc12b4b2621633a
MomohNobert <okhai0112@gmail.com>
2020-11-23 19:23:37 +0100

    optimized bubbleSort with swap condition

--- bubbleSort.js
+++ bubbleSort.js
@@ -17,16 +17,21 @@ function bubbleSortOptimized(arr) {
     const swap = (array, idx1, idx2) => {
         [array[idx1], array[idx2]] = [array[idx2], array[idx1]];
     }
+
+    var noSwaps;
  
     for(var i =  arr.length; i > 0; i--) {
+        noSwaps = true;
         for(var j = 0; j < i - 1; j++) {
             console.log(arr, arr[j], arr[j+1]);
             if (arr[j] > arr[j + 1]) {
                 // SWAP 
-                swap(arr, j, j+1)
+                swap(arr, j, j+1);
+                noSwaps = false;
             }
         }
+        if(noSwaps) break;
     }    
 }
 
-bubbleSortOptimized([10,9,8,7]);
+bubbleSortOptimized([10,7,8,9]);
